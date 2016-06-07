741749c389757688622d85626b25af0af907b15a
wordtinker <wordtinker@yandex.ru>
2016-06-07 10:04:09 +0300

    Add DB initialization

--- Models/Data/SQLiteReader.cs
+++ Models/Data/SQLiteReader.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using System.Data.SQLite;
 using System;
 
 namespace Models
 {
     public class SQLiteReader : FileReader
     {
+        private string connString = "Data Source={0};Version=3;foreign keys=True;";
+
         public override string Extension
         {
             get
@@ -15,9 +17,47 @@ namespace Models
 
         public override bool InitializeFile(string fileName)
         {
+            // TOTO Later : move to foreign key support and cascade delete;
             try
             {
-                // TODO
+                string cString = string.Format(connString, fileName);
+                SQLiteConnection dbConn = new SQLiteConnection(cString);
+                dbConn.Open();
+                
+                string sql = "CREATE TABLE IF NOT EXISTS Accounts(name TEXT, " +
+                    "type TEXT, balance INTEGER, closed INTEGER, exbudget INTEGER)";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                sql = "CREATE TABLE IF NOT EXISTS Transactions(date DATE, " +
+                    "amount INTEGER, info TEXT, acc_id INTEGER, category_id INTEGER)";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                sql = "CREATE TABLE IF NOT EXISTS Categories(name TEXT UNIQUE)";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                sql = "CREATE TABLE IF NOT EXISTS Subcategories(name TEXT, parent TEXT, UNIQUE(name, parent))";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                sql = "CREATE TABLE IF NOT EXISTS Budget(amount INTEGER, " +
+                    "category_id INTEGER, type TEXT, day INTEGER, year INTEGER, month INTEGER";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                dbConn.Close();
                 return true;
             }
             catch (Exception)
@@ -30,7 +70,7 @@ namespace Models
         {
             try
             {
-                // TODO
+                // TODO !!!
                 return true;
             }
             catch (Exception)
