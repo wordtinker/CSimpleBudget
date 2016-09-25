dd4f3343951b9da017daffceb92ca231b86d68ba
wordtinker <wordtinker@yandex.ru>
2016-09-25 13:04:05 +0300

    Set app culture

--- SimpleBudget/App.xaml.cs
+++ SimpleBudget/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 
 namespace SimpleBudget
@@ -13,5 +8,26 @@ namespace SimpleBudget
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Creating a Global culture specific to our application.
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+
+            // Creating the DateTime Information specific to our application.
+            DateTimeFormatInfo dateTimeInfo = new DateTimeFormatInfo();
+
+            // Defining various date and time formats.
+            dateTimeInfo.DateSeparator = "/";
+            dateTimeInfo.LongDatePattern = "dd/MM/yyyy";
+            dateTimeInfo.ShortDatePattern = "dd/MM/yyyy";
+
+            // Setting application wide date time format.
+            cultureInfo.DateTimeFormat = dateTimeInfo;
+
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
     }
 }
